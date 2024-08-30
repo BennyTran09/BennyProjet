ccc84ceacc93c201d10c249653b8102a2c8cec7a
Benny <202144468@collegeahuntsic.qc.ca>
2024-08-30 11:45:57 -0400

    Initial Commit

---
+++ BennyProjet/BennyProjetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BennyProjetApp.swift
+//  BennyProjet
+//
+//  Created by Benny Tran (Étudiant) on 2024-08-30.
+//
+
+import SwiftUI
+
+@main
+struct BennyProjetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
